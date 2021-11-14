bc1a85e38d4e3420f2ae4c96d0734972a38b4353
mrjhong <jfdg206@gmail.com>
2021-11-14 18:10:52 -0500

    loginBarber

---
+++ src/main/java/com/barbershop/crud/security/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.barbershop.crud.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.barbershop.crud.security.entity.Usuario;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
+
+}
